ea527d5556ad2bf619ae1137d52eb88aa7a7df05
Gaelle Londoz <gaelle.londoz@gmail.com>
2018-08-02 18:02:15 +0200

    done

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,88 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+//
+
+
+
+
+
+
+
+$(document).ready(function(){
+  var controller = new ScrollMagic.Controller();
+  var ourScene = new ScrollMagic.Scene({
+    triggerElement: '.expand'
+
+  })
+
+  .setClassToggle('#html-css', 'html-css')
+  .addTo(controller);
+
+});
+
+
+$(document).ready(function(){
+  var controller = new ScrollMagic.Controller();
+  var ourScene = new ScrollMagic.Scene({
+    triggerElement: '.expand'
+
+  })
+
+  .setClassToggle('#ruby', 'ruby')
+  .addTo(controller);
+
+});
+
+
+$(document).ready(function(){
+  var controller = new ScrollMagic.Controller();
+  var ourScene = new ScrollMagic.Scene({
+    triggerElement: '.expand'
+
+  })
+
+  .setClassToggle('#ruby-on-rails', 'ruby-on-rails')
+  .addTo(controller);
+
+});
+
+
+$(document).ready(function(){
+  var controller = new ScrollMagic.Controller();
+  var ourScene = new ScrollMagic.Scene({
+    triggerElement: '.expand'
+  })
+
+  .setClassToggle('#javascript', 'javascript')
+  .addTo(controller);
+
+});
+
+$(document).ready(function(){
+  var controller = new ScrollMagic.Controller();
+  var ourScene = new ScrollMagic.Scene({
+    triggerElement: '.expand'  })
+
+  .setClassToggle('#photoshop', 'photoshop')
+  .addTo(controller);
+
+
+
+});
+
+$(document).ready(function(){
+  var controller = new ScrollMagic.Controller();
+  var ourScene = new ScrollMagic.Scene({
+    triggerElement: '.expand'
+
+  })
+
+
+  .setClassToggle('#illustrator', 'illustrator')
+  .addTo(controller);
+
+
+});
+
+
